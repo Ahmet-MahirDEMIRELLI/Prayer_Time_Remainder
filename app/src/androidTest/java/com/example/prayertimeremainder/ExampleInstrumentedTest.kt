728974bd9cf04ea1ebcf98297693e361161d2f6d
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2024-07-01 11:00:27 +0300

    Theme Updated

--- app/src/androidTest/java/com/example/prayertimeremainder/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/prayertimeremainder/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.ezanokuyucu", appContext.packageName)
+        assertEquals("com.example.prayertimeremainder", appContext.packageName)
     }
 }
